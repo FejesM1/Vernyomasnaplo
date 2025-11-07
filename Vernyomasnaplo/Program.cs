1b7b4162791dabdb2b1e1e49e3e7469dd053274b
Imre <nemethi@kkszki.hu>
2025-11-07 09:21:44 +0100

    Valami elkezdődött :-).

--- Vernyomasnaplo/Program.cs
+++ Vernyomasnaplo/Program.cs
@@ -10,6 +10,68 @@ namespace Vernyomasnaplo
     {
         static void Main(string[] args)
         {
+        bool fut=true;
+        bool selected = false;
+        string[] menupontok = { "Felhasználó regisztrálása", "Felhasználó adatainak módosítása","Felhasználó adatainak megjelenítése", "Felhasználó adatainak törlése", "Beállítások" };
+        int aktualis_menu_pont = 0;
+        int menupontok_szama=menupontok.Length;
+            while (fut)
+            {
+                try
+                {
+                    Console.Clear();
+                    for (int i = 0; i < menupontok_szama; i++)
+                    {
+                        if (aktualis_menu_pont == i)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine(menupontok[i]);
+                            Console.ForegroundColor=ConsoleColor.White;
+                        }
+                        else
+                        {
+                            Console.WriteLine(menupontok[i]);
+
+                        }
+                    }
+                    switch (Console.ReadKey().Key)
+                    {
+                        case ConsoleKey.Enter:
+                            selected = true;
+                            break;
+
+
+                        case ConsoleKey.UpArrow:
+                            if (aktualis_menu_pont > 0)
+                            {
+                                aktualis_menu_pont--;
+                            }
+                            break;
+
+                        case ConsoleKey.DownArrow:
+                            if (aktualis_menu_pont < menupontok_szama-1)
+                            {
+                                aktualis_menu_pont++;
+                            }
+                            break;
+
+
+
+
+                        default:
+                            Console.Beep();
+                            break;
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Hiba történt!");
+                    Console.WriteLine("Szeretné látni a hibát?");
+                    
+                }
+            }
+
         }
     }
 }
